a90b2400d2c2c24ae4fbf5976a3984a68226d8c7
Mirire Okitikpi <93709259+mirireOkitikpi@users.noreply.github.com>
2026-04-10 14:12:26 +0100

    Create camera_smoother.v
    Makes camera movement smoother and game more fluid

---
+++ DSD_project.srcs/camera_smoother.v
@@ -0,0 +1,30 @@
+module camera_smoother (
+    input clk,
+    input rst,
+    input [10:0] logical_y,
+    output reg [10:0] render_y
+);
+
+    localparam PAN_SPEED = 11'd4; 
+
+    // Notice: 'negedge rst' is REMOVED from the sensitivity list.
+    // This makes the reset synchronous, which allows us to load a dynamic variable!
+    always @(posedge clk) begin
+        if (!rst) begin
+            render_y <= logical_y; // Now it will snap instantly on reset!
+        end else begin
+            if (render_y < logical_y) begin
+                if ((logical_y - render_y) < PAN_SPEED)
+                    render_y <= logical_y;
+                else
+                    render_y <= render_y + PAN_SPEED;
+            end 
+            else if (render_y > logical_y) begin
+                if ((render_y - logical_y) < PAN_SPEED)
+                    render_y <= logical_y;
+                else
+                    render_y <= render_y - PAN_SPEED;
+            end
+        end
+    end
+endmodule
